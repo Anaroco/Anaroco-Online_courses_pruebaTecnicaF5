c0e2836a71e3af7e7b5ad587ccc5018074c36564
Anaroco <roblescortes@msn.com>
2022-04-01 20:47:43 +0200

    templete

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -50,7 +50,7 @@
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Cursos Perseo</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Tu camino hacia el éxito</p>
+                    <p class="lead fw-normal text-red-50 mb-0">Tu camino hacia el éxito</p>
                 </div>
             </div>
         </header>
