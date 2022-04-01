bfb52b8d1374cd25363dabffefb64b0d728d702b
Anaroco <roblescortes@msn.com>
2022-04-01 20:19:12 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.template')
+
+@section('content')
+
+@foreach ( $proyectos as $proyecto )
+    
+<div class="col mb-5">
+  <div class="card h-100">
+
+        <!-- Product image-->
+        {{-- <div class= "portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal {{ $proyecto->id }}">
+            <div class= "portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                <div class= "portfolio-item-caption-content text-center text"> --}}
+
+        <img class="card-img-top" src=" {{ $proyecto->imagen }}" alt="tech picture" />
+
+        <!-- Product details-->
+    <div class="card-body p-4">
+    <div class="text-center">
+
+        <!-- Product name-->
+        <h5 class="fw-bolder"> {{ $proyecto->nombre }} </h5>
+
+        <!-- Product reviews-->
+        <div class="d-flex justify-content-center small text-warning mb-2">
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+            <div class="bi-star-fill"></div>
+        </div>
+
+        <!-- Product price-->
+        <p> {{ $proyecto->descripcion }} </p>
+
+    </div>
+    </div>
+
+    <!-- Product actions-->
+    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href=" {{ $proyecto->url }} ">View options</a></div>
+    </div>
+
+  </div>
+</div> 
+
+@endforeach
+
+@endsection 
+
